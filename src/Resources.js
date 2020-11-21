8cff4bcb19b407718f88498d076c8af5def4e965
jackrhumphrey <jack.humphrey@xtra.co.nz>
2020-11-22 10:39:47 +1300

    styling

--- src/Resources.js
+++ src/Resources.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { Input } from "semantic-ui-react";
+import { Input, List } from "semantic-ui-react";
 
 import Header from "./Header";
 
@@ -9,25 +9,35 @@ function Resources() {
         <>
             <Header />
 
-            Voluntary work:
 
-            <a href="https://volunteerwellington.nz">Volunteer Wellington</a>
 
-            <a href="https://www.breastcancerfoundation.org.nz/how-you-can-help/get-involved/volunteer">Breast Cancer Foundation</a>
+            <div className="content">
 
-Free courses:
-——————
-Work connect
-https://www.careers.govt.nz/work-connect/
+            <div className="subheader">Voluntary work</div>
 
-Skilled Newcomers Programme
-https://www.wecc.org.nz/services-and-resources/employment-support
+            <List>
+    <List.Item><a href="https://volunteerwellington.nz">Volunteer Wellington</a></List.Item>
 
-Summer Of Tech
-https://summeroftech.co.nz/
+    <List.Item><a href="https://www.breastcancerfoundation.org.nz/how-you-can-help/get-involved/volunteer">Breast Cancer Foundation</a></List.Item>
 
-Skilled Migrant program
-https://www.wgtn.ac.nz/lals/centres-and-institutes/eli/skilled-migrant-programme
+</List>
+              
+
+            <div className="subheader">Free courses</div>
+
+            <List>
+    <List.Item><a href="https://www.careers.govt.nz/work-connect">Work Connect</a></List.Item>
+
+    <List.Item><a href="https://www.wecc.org.nz/services-and-resources/employment-support">Skilled Newcomers Programme</a></List.Item>
+
+    <List.Item><a href="https://summeroftech.co.nz/">Summer Of Tech</a></List.Item>
+
+    <List.Item><a href="https://www.wgtn.ac.nz/lals/centres-and-institutes/eli/skilled-migrant-programme">Skilled Migrant program</a></List.Item>
+
+
+</List>
+
+</div>
 
 
         </>
